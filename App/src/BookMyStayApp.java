546f8fa2c44bd76e16eff5bd6cb9a42135fc9e29
Sandeep-coder-coder <ss6417@srmist.edu.in>
2026-03-18 14:17:04 +0530

    Use Case 1: Application Entry & Welcome Message

---
+++ App/src/BookMyStayApp.java
@@ -0,0 +1,7 @@
+public class BookMyStayApp {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Book My Stay - Hotel Booking Management System");
+        System.out.println("Version: 1.0");
+        System.out.println("System initialized successfully.");
+    }
+}
